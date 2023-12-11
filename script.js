797a1a180afa1c6750511b8bd40d0714b511d9a4
yoropiko3292 <109505743+yoropiko3292@users.noreply.github.com>
2023-12-11 17:03:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let rule = "ox";
 let M = 1;
 let N = 1;
 let ID = 0;
+let last = 0;
 let data = [];
+let people = [];
 
 //設定の開閉
 document.getElementById("form-open").addEventListener("click",()=>{
@@ -23,31 +25,120 @@ document.getElementById("rule-select").addEventListener("change",()=>{
     let ruleset = document.getElementById("rule-select");
     let inpM = document.getElementById("m");
     let inpN = document.getElementById("n");
+    let setM = document.getElementById("mset");
+    let setN = document.getElementById("nset");
     inpM.value = 5;
     inpN.value = 3;
     if(ruleset.value == "fr"){
-        inpM.style.visibility = "hidden"
-        inpN.style.visibility = "hidden"
+        setM.style.visibility = "hidden"
+        setN.style.visibility = "hidden"
     }else if(ruleset.value == "ox"){
-        inpM.style.visibility = "visible"
-        inpN.style.visibility = "visible"
+        setM.style.visibility = "visible"
+        setN.style.visibility = "visible"
         inpM.value = "";
         inpN.value = "";
     }else if(ruleset.value == "by"){
-        inpM.style.visibility = "visible"
-        inpN.style.visibility = "hidden"
+        setM.style.visibility = "visible"
+        setN.style.visibility = "hidden"
         inpM.value = "";
     }else if(ruleset.value == "ny"){
-        inpM.style.visibility = "visible"
-        inpN.style.visibility = "hidden"
+        setM.style.visibility = "visible"
+        setN.style.visibility = "hidden"
         inpM.value = "";
     }else if(ruleset.value == "ud"){
-        inpM.style.visibility = "visible"
-        inpN.style.visibility = "hidden"
+        setM.style.visibility = "visible"
+        setN.style.visibility = "hidden"
         inpM.value = "";
     }
     rule = ruleset.value;
 });
+
+//要素の追加
+function addMember(){
+
+    let tr = document.createElement("tr");
+    let td = document.createElement("td");
+    let ip = document.createElement("input");
+    let p;
+    let uid = ID;
+    ip.type = "text";
+    ip.className = "nameList";
+    ip.value = "Player" + (uid + 1);
+    if(people.length > uid) ip.value = people[uid];
+    td.appendChild(ip);
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    if(rule == "fr" || rule == "ox" || rule == "by"){ 
+        p = document.createElement("span");
+        p.textContent = " ◯:"
+        td.appendChild(p);
+
+        ip = document.createElement("input");
+        ip.type = "text";
+        ip.id = "tru" + uid;
+        ip.className = "dataset";
+        ip.value = 0;
+        ip.disabled = true;
+        td.appendChild(ip);
+    }
+    if(rule == "fr" || rule == "ox" || rule == "by"){
+        p = document.createElement("span");
+        p.textContent = " ✕:"
+        td.appendChild(p);
+
+        ip = document.createElement("input");
+        ip.type = "text";
+        ip.id = "fal" + uid;
+        ip.className = "dataset";
+        ip.value = 0;
+        ip.disabled = true;
+        td.appendChild(ip);
+    }
+    if(rule == "by" || rule == "ny" || rule == "ud"){
+        p = document.createElement("span");
+        p.textContent = " Score:"
+        td.appendChild(p);
+
+        ip = document.createElement("input");
+        ip.type = "text";
+        ip.className = "dataset";
+        ip.id = "res" + uid;
+        ip.value = 0;
+        ip.disabled = true;
+        td.appendChild(ip);
+    }
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    ip = document.createElement("input");
+    ip.type = "button";
+    ip.value = "◯";
+    ip.addEventListener("click",()=>{pointAdd(uid)});
+    td.appendChild(ip);
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    ip = document.createElement("input");
+    ip.type = "button";
+    ip.addEventListener("click",()=>{pointsub(uid)});
+    ip.value = "✕";
+    td.appendChild(ip);
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    ip = document.createElement("input");
+    ip.type = "text";
+    ip.className = "dataset";
+    ip.id = "state" + uid;
+    ip.disabled = true;
+    ip.value = "-";
+    td.appendChild(ip);
+    tr.appendChild(td);
+
+    table.appendChild(tr);
+    ID++;
+}
 
 //新しいゲームを開始
 document.getElementById("start").addEventListener("click",newGame);
@@ -62,6 +153,7 @@ function newGame(){
     }
     M = Number(document.getElementById("m").value);
     N = Number(document.getElementById("n").value);
+    L = Number(document.getElementById("q").value);
     if(M < 1 || M % 1 != 0){
          alert("無効な値が入力されています");
          return;
@@ -69,6 +161,11 @@ function newGame(){
     if(N < 1 || N % 1 != 0){
          alert("無効な値が入力されています");
          return;
+    }
+    if(L < 1 || L % 1 != 0){
+         document.getElementById("num").textContent = "なし";
+    }else{
+        document.getElementById("num").textContent = L;
     }
     //ルールに応じて要素を配置
     let tmp = "ルール : ";
@@ -78,91 +175,19 @@ function newGame(){
     if(rule == "ny") tmp += M + "NewYork";
     if(rule == "ud") tmp += M + "Up Down";
     document.getElementById("rule").textContent = tmp;
-    while(table.firstChild){
-        table.removeChild(table.firstChild);
+    let temp = document.getElementsByClassName("nameList");
+    people = [];
+    for(let i = 0;i < temp.length;i++){
+        people[i] = temp[i].value;
     }
-    for(let i = 0;i < player;i++){
-        let tr = document.createElement("tr");
-        let td = document.createElement("td");
-        let ip = document.createElement("input");
-        let p;
-        let uid = ID;
-        ip.type = "text";
-        ip.value = "Player" + (i + 1);
-        td.appendChild(ip);
-        tr.appendChild(td);
-
-        td = document.createElement("td");
-        if(rule == "fr" || rule == "ox" || rule == "by"){ 
-            p = document.createElement("span");
-            p.textContent = " ◯:"
-            td.appendChild(p);
-
-            ip = document.createElement("input");
-            ip.type = "text";
-            ip.id = "tru" + uid;
-            ip.className = "dataset";
-            ip.value = 0;
-            ip.disabled = true;
-            td.appendChild(ip);
-        }
-        if(rule == "fr" || rule == "ox" || rule == "by"){
-            p = document.createElement("span");
-            p.textContent = " ✕:"
-            td.appendChild(p);
-
-            ip = document.createElement("input");
-            ip.type = "text";
-            ip.id = "fal" + uid;
-            ip.className = "dataset";
-            ip.value = 0;
-            ip.disabled = true;
-            td.appendChild(ip);
-        }
-        if(rule == "by" || rule == "ny" || rule == "ud"){
-            p = document.createElement("span");
-            p.textContent = " Score:"
-            td.appendChild(p);
-
-            ip = document.createElement("input");
-            ip.type = "text";
-            ip.className = "dataset";
-            ip.id = "res" + uid;
-            ip.value = 0;
-            ip.disabled = true;
-            td.appendChild(ip);
-        }
-        tr.appendChild(td);
-
-        td = document.createElement("td");
-        ip = document.createElement("input");
-        ip.type = "button";
-        ip.value = "◯";
-        ip.addEventListener("click",()=>{pointAdd(uid)});
-        td.appendChild(ip);
-        tr.appendChild(td);
-
-        td = document.createElement("td");
-        ip = document.createElement("input");
-        ip.type = "button";
-        ip.addEventListener("click",()=>{pointsub(uid)});
-        ip.value = "✕";
-        td.appendChild(ip);
-        tr.appendChild(td);
-
-        td = document.createElement("td");
-        ip = document.createElement("input");
-        ip.type = "text";
-        ip.className = "dataset";
-        ip.id = "state" + uid;
-        ip.disabled = true;
-        ip.value = "-";
-        td.appendChild(ip);
-        tr.appendChild(td);
-
-        table.appendChild(tr);
-        ID++;
+    while(table.childElementCount > 0){
+        table.removeChild(table.lastChild);
     }
+    ID = 0;
+    while(table.childElementCount != player){
+        addMember();
+    }
+
     //最初の履歴を追加
     let values = [];
     let datasets = document.getElementsByClassName("dataset");
@@ -192,8 +217,10 @@ function pointsub(x){
 
 //点数、勝敗判定 引数x=id,正答ならy=1,誤答ならy=-1
 function pointRes(x,y){
+    let lastText = document.getElementById("num");
     let datasets = document.getElementsByClassName("dataset");
     let time = new Date();
+    if(y != 0){
     let cont = true;
     let win = false;
     let truTxt = document.getElementById("tru" + x);
@@ -230,22 +257,32 @@ function pointRes(x,y){
         if(truTxt.value >= M) win = true;
         if(falTxt.value >= N) cont = false;
     }
-    if(!cont){
+    if(!cont && (stateTxt.value == "-" || stateTxt.value == "誤答1")){
         stateTxt.value = "失格  " + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
     }
-    if(win){
+    if(win && stateTxt.value == "-"){
         stateTxt.value = "Win!  " + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+    }
     }
     let values = [];
     for(let i = 0;i < datasets.length;i++){
         values.push(datasets[i].value);
     }
     data[data.length] = values;
+    if(lastText.textContent != "なし") lastText.textContent = Number(lastText.textContent) - 1;
+    if(lastText.textContent == 0)setTimeout(()=>{alert("終了!")},100);
 }
+
+//スキップ
+document.getElementById("skip").addEventListener("click",()=>{
+    pointRes(0,0);
+});
 
 //1手前の状態に戻す
 document.getElementById("undo").addEventListener("click",()=>{
     if(data.length > 1){
+        let lastText = document.getElementById("num");
+        if(lastText.textContent != "なし") lastText.textContent = Number(lastText.textContent) + 1;
         let datasets = document.getElementsByClassName("dataset");
         for(let i = 0;i < datasets.length;i++){
             datasets[i].value = data[data.length - 2][i];
